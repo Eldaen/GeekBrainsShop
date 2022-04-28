6c2425ec2bfba670d65b9fa5d18302f6c396f54b
Sizov Denis <sv_skatch@bk.ru>
2022-04-28 13:42:02 +0300

    Added registration test

--- GeekBrainsShopTests/UserAuthTests.swift
+++ GeekBrainsShopTests/UserAuthTests.swift
@@ -24,6 +24,7 @@ class UserAuthTests: XCTestCase {
     }
 	
 	/// Проверяет авторизацию с корректными данными
+	/// Корректность данных проверить не можем, смотрим только успешный ответ
 	func testCorrectLogin() throws {
 		// Given
 		var user: User?
@@ -49,12 +50,47 @@ class UserAuthTests: XCTestCase {
 			guard let user = user else {
 				XCTFail()
 				return
-			}	
+			}
 			
 			XCTAssertEqual(user.id, 123)
 			XCTAssertEqual(user.login, "geekbrains")
 			XCTAssertEqual(user.name, "John")
 			XCTAssertEqual(user.lastname, "Doe")
+		}
+	}
+	
+	/// Проверяет успешную регистрацию нового пользователя
+	/// Тут бы проверить, что пользователь вообще в принципе зарегистрировался, но у нас этого нет пока
+	func testCorrectRegister() throws {
+		// Given
+		var message = ""
+		let validatorExpectation = expectation(description: #function)
+		
+		// When
+		auth.register(
+			username: "Somebody",
+			password: "mypassword",
+			email: "some@some.ru",
+			gender: .male,
+			creditCard: "9872389-2424-234224-234",
+			bio: "This is good! I think I will switch to another language"
+		) { response in
+			switch response.result {
+			case .success(let result):
+				message = result.userMessage
+				validatorExpectation.fulfill()
+			case .failure(let error):
+				print(error)
+			}
+		}
+		
+		//Then
+		waitForExpectations(timeout: 1.0) { error in
+			if error != nil {
+				XCTFail()
+			}
+			
+			XCTAssertEqual(message, "Регистрация прошла успешно!")
 		}
 	}
 }
